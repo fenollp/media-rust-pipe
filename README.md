095e4a463b2ded1d198080cff26dad64c72e8e3b
Pierre Fenoll <pierrefenoll@gmail.com>
2024-04-27 09:04:12 +0200

    missing absl?
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 - [ ] example `Calculator`
 
 \* needs `protoc mediapipe/mediapipe/framework/formats/image_format.proto --cpp_out=.` and  `sudo cargo build` on Mac Os for bazel artifacts
+
+Mediapipe release closest to first commit
+https://github.com/ahirner/media-rust-pipe/commit/9171594f5b753ef7bb7ea8e9ec4127de2a19f13c
+https://github.com/google/mediapipe/releases/tag/v0.7.5
+	https://github.com/google/mediapipe/commit/5d028d923b52fd95bd49642a9a4a162f57ae260e
--- build.rs
+++ build.rs
@@ -3,6 +3,9 @@ use std::env;
 use std::path::Path;
 
 fn main() {
+    env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
+    // TODO: run protoc mediapipe/mediapipe/framework/formats/image_format.proto --cpp_out=.
+
     let manifest_dir = env::var_os("CARGO_MANIFEST_DIR").unwrap();
     let mediapipe_include_dir = Path::new(&manifest_dir).join("mediapipe");
     CFG.exported_header_dirs.push(&mediapipe_include_dir);
@@ -15,4 +18,6 @@ fn main() {
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=mediapipe-bind.h");
     println!("cargo:rerun-if-changed=mediapipe-bind.cc");
+
+    Ok(())
 }
